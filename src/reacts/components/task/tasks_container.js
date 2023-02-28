ffb51763df94bc213a5d137f641e76cf3ec684ad
raman suliman <75031065+RamanSuliman@users.noreply.github.com>
2023-02-28 22:35:02 +0000

    Defining task containers sections with identifier and adding default tasks.

--- src/reacts/components/task/tasks_container.js
+++ src/reacts/components/task/tasks_container.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Task} from './task';
+import {Popup} from './task_controller';
 
 export const TaskContainer = () =>
 {
@@ -14,16 +15,13 @@ export const TaskContainer = () =>
 function Container() {
     return (
         <div className="container">
-            <div className="section">
-                <h2 className="title">Section 1</h2>
+            <div className="section" id="component1">
                 <Component1 />
             </div>
-            <div className="section">
-                <h2 className="title">Section 2</h2>
+            <div className="section" id="component2">
                 <Component2 />
             </div>
-            <div className="section">
-                <h2 className="title">Section 3</h2>
+            <div className="section" id="component3">
                 <Component3 />
             </div>
         </div>
@@ -34,9 +32,11 @@ function Component1() {
     return (
         <div className="component">
             {/* Component 1 content goes here */}
-            <p className="title">...</p>
             <div>
-                <Task/>
+                <Task
+                    title="Task Header"
+                    descr="This is description for the task"
+                    btn_text="..."/>
             </div>
         </div>
     );
@@ -46,10 +46,13 @@ function Component2() {
     return (
         <div className="component">
             {/* Component 2 content goes here */}
-            <p className="title">...</p>
             <div>
-                <Task/>
+                <Task
+                    title="Task Header 2"
+                    descr="This is description for the task"
+                    btn_text="..."/>
             </div>
+            <Popup/>
         </div>
     );
 }
@@ -58,9 +61,11 @@ function Component3() {
     return (
         <div className="component">
             {/* Component 3 content goes here */}
-            <p className="title">...</p>
             <div>
-                <Task/>
+                <Task
+                    title="Task Header 3"
+                    descr="This is description for the task"
+                    btn_text="..."/>
             </div>
         </div>
     );
