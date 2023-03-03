1b35e4b5ad75adba659306403433e00c9f5d5f6e
raman suliman <75031065+RamanSuliman@users.noreply.github.com>
2023-03-03 23:58:39 +0000

    Removing redundant commented code.

--- src/reacts/components/modules/task/controller.js
+++ src/reacts/components/modules/task/controller.js
@@ -4,10 +4,10 @@ import {FetchTasks} from "../ajax_calls/ajaxer";
 
 export function SectionRender() {
     //Receive api data and convert it into json object.
-    //const {sections} = JSON.parse(data());
+    const {sections} = JSON.parse(data());
 
     //FetchTasks("http://localhost:8080/ManagmentToolApplication/api/tasks");
-
+    /*
     const { isLoading, tasks, error, responseState} = FetchTasks("http://localhost:8080/ManagmentToolApplication/api/tasks");
     if (isLoading) {
         return <div>Loading...</div>;
@@ -16,11 +16,12 @@ export function SectionRender() {
         return <div>Error fetching data. {responseState}</div>;
     }else
         console.log("----------------- Tasks: " + tasks.sections[0].title);
+    */
 
     return (
         <div className="taskContainer">
             {/*Map through the sections*/}
-            {tasks.sections.map(section => {
+            {sections.map(section => {
                 // For each section, add the title and
                 return <Section key={section.id} id={section.id} title={section.title} tasks={section.tasks} />
             })}
@@ -38,7 +39,6 @@ function Section({id, title, tasks}) {
         </div>
     );
 }
-
 
 function data(){
     const json = {
@@ -102,111 +102,3 @@ function data(){
     // Convert into json
     return JSON.stringify(json);
 }
-
-
-/*
-
-function Tasks() {
-    const tasks = [
-        { title: "Task Header 1", descr: "This is description for task 1", btn_text: "..." },
-        { title: "Task Header 2", descr: "This is description for task 2", btn_text: "..." },
-        { title: "Task Header 3", descr: "This is description for task 3", btn_text: "..." }
-    ];
-
-    const taskComponents = [];
-
-    tasks.forEach((task, index) => {
-        taskComponents.push(
-            <Task
-                key={index}
-                title={task.title}
-                descr={task.descr}
-                btn_text={task.btn_text}
-            />
-        );
-    });
-
-    return <div>{taskComponents}</div>;
-}
-
-
-
-
-export function Popup()
-{
-    const [showPopup, setShowPopup] = useState(false);
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [taskType, setTaskType] = useState('');
-    const handleAddTask = () => {
-        console.log(taskType)
-        const targetSection = getTargetSection(taskType);
-        if (targetSection) {
-            AddNewTask({ title, description, type: taskType, targetSection});
-            setShowPopup(false);
-        } else {
-            console.error(`Invalid task type: ${taskType}`);
-        }
-    };
-
-    const getTargetSection = (type) => {
-        switch (type) {
-            case 'To Do':
-                return document.querySelector('#section_ToDo');
-            case 'In Progress':
-                return document.querySelector('#section_InProgress');
-            case 'Done':
-                return document.querySelector('#section_Done');
-            default:
-                return null;
-        }
-    };
-
-    return (
-        <div>
-            <button onClick={() => setShowPopup(true)}>Add Task</button>
-            {showPopup && (
-                <div className="popup">
-                    <h2>Add Task</h2>
-                    <label>
-                        Title:
-                        <input
-                            type="text"
-                            value={title}
-                            onChange={(event) => setTitle(event.target.value)}
-                        />
-                    </label>
-                    <label>
-                        Description:
-                        <textarea
-                            value={description}
-                            onChange={(event) => setDescription(event.target.value)}
-                        />
-                    </label>
-                    <label>
-                        Task Type:
-                        <select value={taskType} onChange={(event) => setTaskType(event.target.value)}>
-                            <option value="">Select a task type</option>
-                            <option value="To Do">To Do</option>
-                            <option value="In Progress">In Progress</option>
-                            <option value="Done">Done</option>
-                        </select>
-                    </label>
-                    <button onClick={handleAddTask}>Add Task</button>
-                    <button onClick={() => setShowPopup(false)}>Cancel</button>
-                </div>
-            )}
-        </div>
-    );
-}
-
-
-export function AddNewTask({ title, description, type, targetSection }) {
-    const task = <Task title={title} descr={description} type={type} />;
-    const container = document.createElement('div');
-    //Render the task into the container.
-    const root = ReactDOM.createRoot(container);
-    root.render(task);
-    targetSection.appendChild(container);
-}
- */
