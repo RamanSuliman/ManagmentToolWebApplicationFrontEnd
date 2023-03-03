bdd4fcfa2bc60c41d31f7ef488fac5607f9fc9ab
raman suliman <75031065+RamanSuliman@users.noreply.github.com>
2023-03-03 23:59:12 +0000

    Integrating an edit button and creating a relation with edit menu component.

--- src/reacts/components/modules/task/task.js
+++ src/reacts/components/modules/task/task.js
@@ -1,12 +1,25 @@
+import React, {useState} from "react";
+import {EditPopup} from "./menus";
 
 export const Task = ({id, title, description, btn_text, type}) =>
 {
     const taskClassName = getTaskClassName(type);
+    const [task, setTask] = useState({id: id, title: title, description: description, type: type});
+
+    const [isEditShowing, setEditShow] = useState(false);
+    //const task = ;
+    //console.log("Within the task: " + task.id)
+    const editEventHandler = () =>{
+        setEditShow(true);
+    }
     return (
         <div id={id} className={taskClassName}>
             <h2>{title}</h2>
             <p>{description}</p>
-            {/*<MenuButton buttonText={btn_text} menuComponent={<Menu/>} /> */}
+            <button onClick={editEventHandler}>Edit</button>
+            {isEditShowing && (
+                <EditPopup task={task} setTask={setTask} isEditShowing={isEditShowing} setEditShow={setEditShow} />
+            )}
         </div>
     );
 };
